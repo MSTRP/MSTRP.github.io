f662bde1fddf0c9d9ec151e9a8ba96c2452783e5
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-01-22 18:55:50 +0000

    hotkeynav debug
    index of N in back case adjusted present

--- JS/Library/IPlibrary.js
+++ JS/Library/IPlibrary.js
@@ -1667,11 +1667,11 @@ var hotkeyNavigate = function (question) {
 
             //get direction as a string
             let direction =
-                (pressedKeys.indexOf(17) > -1 && pressedKeys.indexOf(18) > -1 && pressedKeys.indexOf(80) > -1 && pressedKeys.indexOf(78) === 0 && jQuery("#PreviousButton").length)
-                    //if CTRL + ALT + N are pressed and the next button is present:
+                (pressedKeys.indexOf(17) > -1 && pressedKeys.indexOf(18) > -1 && pressedKeys.indexOf(80) > -1 && pressedKeys.indexOf(78) === -1 && jQuery("#PreviousButton").length)
+                    //if CTRL + ALT + P are pressed and the next button is present:
                     ? "back"
                     : (pressedKeys.indexOf(17) > -1 && pressedKeys.indexOf(18) > -1 && pressedKeys.indexOf(78) > -1 && pressedKeys.indexOf(80) === -1 && jQuery("#NextButton").length)
-                        //if CTRL + ALT + P are pressed and the previous button is present:
+                        //if CTRL + ALT + N are pressed and the previous button is present:
                         ? "next" : "";
 
             if (all_inputs < matrixThreshold) {
