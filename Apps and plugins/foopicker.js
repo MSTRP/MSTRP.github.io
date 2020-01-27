0a49a4fe94ea209a2e11d8cf6db706ed2ec6bd9f
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-01-27 16:53:59 +0000

    foopicker debug
    using bottom figure and adjusting for height of element instead of top figure

--- Apps and plugins/foopicker.js
+++ Apps and plugins/foopicker.js
@@ -36,7 +36,7 @@ var FooPicker = (function () {
       if (pickerField) {
         var datepicker = pickerField.getBoundingClientRect();
         var left = datepicker.left;
-        var top = datepicker.top; /* + 20; */
+        var top = datepicker.bottom + datepicker.height + 5; /* + 20; */
         console.log("top: ", top );
         if (pickerDiv) {
           pickerDiv.style.position = 'fixed';
