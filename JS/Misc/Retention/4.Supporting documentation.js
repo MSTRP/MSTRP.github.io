15f72c3c455425b1ae813d79b7320b7811ed6d26
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-06-01 19:11:38 +0100

    retention: supporting docs
    button functions updated

--- JS/Misc/Retention/4.Supporting documentation.js
+++ JS/Misc/Retention/4.Supporting documentation.js
@@ -1,16 +1,16 @@
-Qualtrics.SurveyEngine.addOnload(function()
-{
-	//-----------Buttons:
-    nextbuttonDefault(sections.Submit2);
-    prevbuttonDefault("Section 3: Supporting translation");
+Qualtrics.SurveyEngine.addOnload(function () {
+    //-----------Buttons:
+    let next = "Submit2";
+    let details = "Supporting";
+
+    loadSwitch("next", [details, next],qFilters); //NEXT on load
+    clickSwitch([next, details], '#QID156', '1')//click switcher
+    prevbuttonDefault("Section 3: Supporting Translation");
 });
 
-Qualtrics.SurveyEngine.addOnReady(function()
-{
-	/*Place your JavaScript here to run when the page is fully displayed*/
+Qualtrics.SurveyEngine.addOnReady(function () {
+    /*Place your JavaScript here to run when the page is fully displayed*/
 });
-
-Qualtrics.SurveyEngine.addOnUnload(function()
-{
-	/*Place your JavaScript here to run when the page is unloaded*/
+Qualtrics.SurveyEngine.addOnUnload(function () {
+    /*Place your JavaScript here to run when the page is unloaded*/
 });
