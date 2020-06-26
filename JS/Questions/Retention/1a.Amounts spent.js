55c19d496538c721459d006ef3d08910ae6e41f2
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-06-26 16:04:08 +0100

    Retention unspent amount
    obsolete question deleted (HTML/JS)
    
    Unspect and spent amount questions updated and renumbered

--- JS/Questions/Retention/1a.Amounts spent.js
+++ JS/Questions/Retention/1a.Amounts spent.js
@@ -1,10 +1,14 @@
 Qualtrics.SurveyEngine.addOnload(function () {
         /*Place your JavaScript here to run when the page loads*/
+        nextbuttonDefault("Activity summary");
 });
 
 Qualtrics.SurveyEngine.addOnReady(function () {
         //---------Currency formatting:
         currencyPounds1(this);
+
+        //---------Calc unspent amounts
+        fieldCalc("#QID235", "#QID236", "${e://Field/Retained%20amount}");
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function () {
