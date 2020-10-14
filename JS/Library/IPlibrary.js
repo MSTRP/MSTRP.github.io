00eb69cf7ed6720e9bf9f55a533cb38ccd031550
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-10-14 17:05:13 +0100

    increment
    increment

--- JS/Library/IPlibrary.js
+++ JS/Library/IPlibrary.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //version tracking
-var version = "live Beta update " + '1.2.0.1';//increment me when publishing changes
+var version = "live Beta update " + '1.2.0.2';//increment me when publishing changes
 console.log("Version: ", version);
 
 
