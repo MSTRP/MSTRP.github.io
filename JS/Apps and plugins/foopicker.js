1de024f00fe3320928d1288e82128a278d899ea0
MSTRP <51442122+MSTRP@users.noreply.github.com>
2020-11-19 15:39:36 +0000

    Update foopicker.js

--- JS/Apps and plugins/foopicker.js
+++ JS/Apps and plugins/foopicker.js
@@ -5,7 +5,7 @@
  * License: MIT
  * URL: https://github.com/yogasaikrishna/foopicker
  */
-var fooVersion = 2.1;
+var fooVersion = 2.2;
 console.log("Date Picker version: ", fooVersion);
 var FooPicker = (function () {
   'use strict';
@@ -37,12 +37,12 @@ var FooPicker = (function () {
       if (pickerField) {
         var datepicker = pickerField.getBoundingClientRect();
         var left = datepicker.left;
-        var top = datepicker.top /* + window.scrollY + 220; */
+        var top = datepicker.top + pickerDiv.style.height /* + window.scrollY + 220; */
         console.log("cell top: ", datepicker.top);
         var windowWidth = (window.innerWidth == undefined) ? document.documentElement.clientWidth : window.innerWidth;
         if (pickerDiv) {
           pickerDiv.style.position = 'fixed';
-          pickerDiv.style.bottom = top + /* 10 + */ 'px';
+          pickerDiv.style.top = top + /* 10 + */ 'px';
           console.log("foopicker bottom: ", pickerDiv.style.bottom);
           pickerDiv.style.left = /* (pickerDiv.style.right > windowWidth) ? left  - (pickerDiv.style.right - windowWidth) : */ left + /* 91 + */ 'px';
           pickerDiv.style.zIndex = '99999';
